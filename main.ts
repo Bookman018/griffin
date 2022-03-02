dad203be9a849986ccef5cc0c6fb9cb84cebc7d2
Bookman018 <88204971+Bookman018@users.noreply.github.com>
2022-03-02 15:21:17 -0800

    Changed some stuff

--- main.ts
+++ main.ts
@@ -1,27 +1,37 @@
 input.onButtonPressed(Button.A, function () {
-    hummingbird.setPositionServo(FourPort.Three, 180)
-    hummingbird.setPositionServo(FourPort.Four, 180)
-    basic.pause(100)
-    hummingbird.setPositionServo(FourPort.Three, 0)
-    hummingbird.setPositionServo(FourPort.Four, 0)
+    hummingbird.setRotationServo(FourPort.One, 100)
+    hummingbird.setRotationServo(FourPort.Two, -100)
+    basic.pause(1000)
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
 })
 function Lights () {
     hummingbird.setTriLED(
     TwoPort.One,
-    200,
-    191,
-    128
+    212,
+    175,
+    55
     )
     hummingbird.setTriLED(
     TwoPort.Two,
-    200,
-    191,
-    128
+    212,
+    175,
+    55
     )
-    hummingbird.setLED(ThreePort.One, 100)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . # . #
+        # . . . #
+        . # # # .
+        `)
 }
 input.onButtonPressed(Button.B, function () {
-	
+    hummingbird.setRotationServo(FourPort.One, -100)
+    hummingbird.setRotationServo(FourPort.Two, 100)
+    basic.pause(1000)
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
 })
 function soundSensor () {
     if (hummingbird.getSensor(SensorType.Sound, ThreePort.Two) > 99) {
@@ -30,13 +40,24 @@ function soundSensor () {
         basic.pause(100)
         hummingbird.setPositionServo(FourPort.Three, 0)
         hummingbird.setPositionServo(FourPort.Four, 0)
+        music.playMelody("A A A A A A A A ", 160)
     }
 }
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    hummingbird.setRotationServo(FourPort.One, 100)
+    hummingbird.setRotationServo(FourPort.Two, 100)
+    basic.pause(1000)
+    hummingbird.setRotationServo(FourPort.One, 0)
+    hummingbird.setRotationServo(FourPort.Two, 0)
+})
 function distanceSensor () {
-    if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 0) {
-        music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 300)
+    if (hummingbird.getSensor(SensorType.Distance, ThreePort.One) < 1) {
+        hummingbird.setRotationServo(FourPort.One, -100)
+        hummingbird.setRotationServo(FourPort.Two, -100)
+        hummingbird.setLED(ThreePort.One, 100)
     }
 }
+hummingbird.setLED(ThreePort.One, 0)
 hummingbird.setPositionServo(FourPort.Three, 0)
 hummingbird.setPositionServo(FourPort.Four, 0)
 hummingbird.startHummingbird()
